f94931c6a4e4fb506c974caa1af4ce74f88ee8a0
hanmingliu2 <altars.corgis-0h@icloud.com>
2025-04-21 13:15:16 -0400

    add folder storing scraped novels

--- src/utils/path.py
+++ src/utils/path.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 CONFIG_FOLDER = PROJECT_ROOT / "src" / "config"
 UTILS_FOLDER = PROJECT_ROOT / "src" / "utils"
 DATA_FOLDER = PROJECT_ROOT / "src" / "data"
+NOVELS_FOLDER = DATA_FOLDER / "novels"
