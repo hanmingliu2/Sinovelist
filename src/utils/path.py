656d20a9b859cda6bf2b854161c3114cf58b9f0e
hanmingliu2 <altars.corgis-0h@icloud.com>
2025-04-24 00:13:01 -0400

    add metadata csv to path.py

--- src/utils/path.py
+++ src/utils/path.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 CONFIG_FOLDER = PROJECT_ROOT / "src" / "config"
 UTILS_FOLDER = PROJECT_ROOT / "src" / "utils"
 DATA_FOLDER = PROJECT_ROOT / "src" / "data"
 NOVELS_FOLDER = DATA_FOLDER / "novels"
+METADATA_CSV = DATA_FOLDER / "metadata.csv"
