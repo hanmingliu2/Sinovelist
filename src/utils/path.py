772ef56b52d241fd50e3170a1e45f4597b669dc7
hanmingliu2 <altars.corgis-0h@icloud.com>
2025-05-31 17:19:11 -0400

    add models folder for convenience

--- src/utils/path.py
+++ src/utils/path.py
@@ -4,5 +4,6 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 CONFIG_FOLDER = PROJECT_ROOT / "src" / "config"
 UTILS_FOLDER = PROJECT_ROOT / "src" / "utils"
 DATA_FOLDER = PROJECT_ROOT / "src" / "data"
+MODELS_FOLDER = PROJECT_ROOT / "src" / "models"
 NOVELS_FOLDER = DATA_FOLDER / "novels"
 NOVELS_METADATA = DATA_FOLDER / "novel_metadata.csv"
